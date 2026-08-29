09afa16a677709f396e6f914f7425339e092c2f5
Anastasia Melnikova <melnikova-online@yandex.ru>
2026-08-29 14:21:52 +0500

    Update files

--- functions.py
+++ functions.py
@@ -1,2 +1,13 @@
-def greet(name):
-    return f"Привет, {name}!"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def print_author():
+    author = os.getenv("AUTHOR", "Anastasia Melnikova")
+    print(f"Автор проекта: {author}")
+
+
+if __name__ == "__main__":
+    print_author()
